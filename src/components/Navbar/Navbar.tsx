4503d9216fdb1b6626862210799dcb718a2ccb11
rokaskvasas <rokas.kvasas@gmail.com>
2023-03-07 21:04:21 +0200

    Created layout and navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex>
+      
+    </Flex>
+  )
+};
+export default Navbar;
