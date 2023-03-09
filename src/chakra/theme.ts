24f103bc26ab085df2f805938d000dabe1891688
rokaskvasas <rokas.kvasas@gmail.com>
2023-03-09 20:19:03 +0200

    - create button theme

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
+import { Button } from "./button";
 
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
@@ -13,14 +14,14 @@ export const theme = extendTheme({
   fonts: {
     body: "Open Sans, sans-serif",
   },
-  styles :{
+  styles: {
     global: () => ({
-        body: {
-            bg: "gray.200",
-        },
+      body: {
+        bg: "gray.200",
+      },
     }),
   },
   components: {
-        // Button
-  }
+    Button,
+  },
 });
