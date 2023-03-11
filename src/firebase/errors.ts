1a3d51a002214b9466010bf2e6caa440fdee59b8
rokaskvasas <rokas.kvasas@gmail.com>
2023-03-11 14:56:33 +0200

    - Added few more firebase error messages

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -2,7 +2,10 @@ export const FIREBASE_ERRORS = {
   "Firebase: Error (auth/email-already-in-use).":
     "A user with that email already exists!",
   "Firebase: Error (auth/user-not-found).": "Invalid email or password",
-  "Firebase:Error (auth/wrong-password).": "Invalid email or password",
+  "Firebase: Error (auth/wrong-password).": "Invalid email or password",
   "Firebase: Password should be at least 6 characters (auth/weak-password).":
     "Too weak bitch.",
+  "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).":
+    "NANY!!!",
+  "Firebase: Error (auth/operation-not-allowed).": "Operation not allowed!",
 };
