0cb2251321b1948eca848af6a675f82c9e4cbfcc
rokaskvasas <rokas.kvasas@gmail.com>
2023-03-20 18:57:43 +0200

    Created TabItems and newPostForm skeleton

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,24 +1,20 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
 
-type PageContentProps = {};
+type PageContentProps = {
+  children: any;
+};
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
   console.log("Here is children", children);
   return (
-    <Flex justify="center" p="16px 0px" border="1px solid red">
-      <Flex
-        justify="center"
-        maxWidth="860px"
-        width="95%"
-        border="1px solid black"
-      >
+    <Flex justify="center" p="16px 0px">
+      <Flex justify="center" maxWidth="860px" width="95%">
         {/* LHS */}
         <Flex
           direction="column"
           width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
-          border="1px solid blue"
         >
           {children && children[0 as keyof typeof children]}
         </Flex>
@@ -28,7 +24,6 @@ const PageContent: React.FC<PageContentProps> = ({ children }) => {
           direction="column"
           display={{ base: "none", md: "flex" }}
           flexGrow={1}
-          border="1px solid orange"
         >
           {children && children[1 as keyof typeof children]}
         </Flex>
