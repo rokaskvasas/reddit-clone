439d8d2415c4ee0490207fe9baf839c22cefc7a0
rokaskvasas <rokas.kvasas@gmail.com>
2023-03-26 12:46:26 +0300

    Implemented logic for creating new post

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,4 +1,13 @@
-import { Flex, Icon } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  CloseButton,
+  Flex,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { BiPoll } from "react-icons/bi";
@@ -6,8 +15,22 @@ import { IoDocumentText, IoImageOutline } from "react-icons/io5";
 import TabItem from "./TabItem";
 import TextInputs from "./PostForm/TextInputs";
 import ImageUpload from "./PostForm/ImageUpload";
+import { Post } from "@/atoms/postsAtom";
+import { User } from "firebase/auth";
+import { useRouter } from "next/router";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { firestore, storage } from "@/firebase/clientApp";
+import { getDownloadURL, ref, uploadString } from "@firebase/storage";
 
-type NewPostFromProps = {};
+type NewPostFromProps = {
+  user: User;
+};
 
 const formTabs: TabItem[] = [
   {
@@ -36,9 +59,9 @@ export type TabItem = {
   title: string;
   icon: typeof Icon.arguments;
 };
-const NewPostForm: React.FC<NewPostFromProps> = () => {
+const NewPostForm: React.FC<NewPostFromProps> = ({ user }) => {
+  const router = useRouter();
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
-
   const [textInputs, setTextInputs] = useState({
     title: "",
     body: "",
@@ -46,7 +69,48 @@ const NewPostForm: React.FC<NewPostFromProps> = () => {
 
   const [selectedFile, setSelectedFile] = useState<string>();
   const [loading, setLoading] = useState(false);
-  const handleCreatePost = async () => {};
+  const [error, setError] = useState(false);
+  const handleCreatePost = async () => {
+    /*  Create new post object => type post
+     *  store the post in db
+     *  check for selectedFile
+     *  store in storage => getDownloadURL ( return imageURL)
+     *  update post doc by adding imageURL
+     *  redirect the user back to the communityPage using the router.
+     *
+     */
+    const { communityId } = router.query;
+    const newPost: Post = {
+      communityId: communityId as string,
+      creatorId: user.uid,
+      creatorDisplayName: user.email!.split("@")[0],
+      title: textInputs.title,
+      body: textInputs.body,
+      numberOfComments: 0,
+      voteStatus: 0,
+      createdAt: serverTimestamp() as Timestamp,
+    };
+    setLoading(true);
+    try {
+      const postDocRef = await addDoc(collection(firestore, "posts"), newPost);
+
+      if (selectedFile) {
+        const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+        await uploadString(imageRef, selectedFile, "data_url");
+        const downloadURL = await getDownloadURL(imageRef);
+
+        await updateDoc(postDocRef, {
+          imageURL: downloadURL,
+        });
+      }
+    } catch (error: any) {
+      console.log("HandleCreatePost error", error.message);
+      setError(true);
+    }
+    setLoading(false);
+
+    //router.back();
+  };
 
   const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
@@ -104,6 +168,12 @@ const NewPostForm: React.FC<NewPostFromProps> = () => {
           />
         )}
       </Flex>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <Text mr={2}>Error creating post!</Text>
+        </Alert>
+      )}
     </Flex>
   );
 };
