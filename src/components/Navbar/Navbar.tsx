553be535a0aed483f5f65bba459947b75ee9d784
rokaskvasas <rokas.kvasas@gmail.com>
2023-04-01 12:37:31 +0300

    Added on click to home page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
+import { defaultMenuItem } from "@/atoms/directoryMenuAtom";
 import { auth } from "@/firebase/clientApp";
+import useDirectory from "@/hooks/useDirectory";
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -8,6 +10,7 @@ import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
+  const { onSelectMenuItem } = useDirectory();
   return (
     <Flex
       bg="white"
@@ -19,6 +22,8 @@ const Navbar: React.FC = () => {
         align="center"
         width={{ base: "40px", md: "auto" }}
         mr={{ base: 0, md: 2 }}
+        onClick={() => onSelectMenuItem(defaultMenuItem)}
+        cursor="pointer"
       >
         <Image src="/images/redditFace.svg" height="30px" />
         <Image
