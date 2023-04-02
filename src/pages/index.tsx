ed2ddf273075587334edafaa45b3e27e768b9b53
rokaskvasas <rokas.kvasas@gmail.com>
2023-04-02 12:43:37 +0300

    Impl of home page for logged out users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,93 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import { Post } from "@/atoms/postsAtom";
+import CreatePostLink from "@/components/Community/CreatePostLink";
+import PageContent from "@/components/Layout/PageContent";
+import PostItem from "@/components/Posts/PostItem";
+import PostLoader from "@/components/Posts/PostLoader";
+import { auth, firestore } from "@/firebase/clientApp";
+import usePosts from "@/hooks/usePosts";
+import { Stack } from "@chakra-ui/react";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-const inter = Inter({ subsets: ["latin"] });
+const Home: NextPage = () => {
+  const [user, loadingUser] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onDeletePost,
+    onSelectPost,
+    onVote,
+  } = usePosts();
+  const buildUserHomeFeed = () => {};
 
-export default function Home() {
-  return <div>s</div>;
-}
+  const buildNoUserHomeFeed = async () => {
+    setLoading(true);
+
+    try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }));
+    } catch (error) {
+      console.log("BuildNoUserHomeFeed error", error);
+    }
+    setLoading(false);
+  };
+
+  const getUserPostVotes = () => {};
+
+  // useEffects
+
+  useEffect(() => {
+    if (!user && !loadingUser) buildNoUserHomeFeed();
+  }, [user, loadingUser]);
+  return (
+    <PageContent>
+      <>
+        <CreatePostLink />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateValue.posts.map((post) => (
+              <PostItem
+                post={post}
+                onDeletePost={onDeletePost}
+                key={post.id}
+                onSelectPost={onSelectPost}
+                onVote={onVote}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId === post.id
+                  )?.voteValue
+                }
+                userIsCreator={user?.uid === post.creatorId}
+                homePage
+              />
+            ))}
+          </Stack>
+        )}
+      </>
+      <></>
+    </PageContent>
+  );
+};
+export default Home;
